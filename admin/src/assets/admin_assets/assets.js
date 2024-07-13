712519d304c85851baf633dfae4e21b0197702a2
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-07-13 18:35:03 +0530

    feat: Frontend for admin side

---
+++ admin/src/assets/admin_assets/assets.js
@@ -0,0 +1,17 @@
+import logo_admin from './logo_admin'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets ={
+    logo_admin,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
+
+export const url = 'http://localhost:4000'
